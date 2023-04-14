9236342e09a9a71a9631d86ed391a58f9cea574d
Jeffrey <jeffrey.cochachin@gmail.com>
2023-04-14 15:23:18 -0500

    Tarea04

---
+++ app.js
@@ -0,0 +1,13 @@
+const jsonData = '{"parent":{"name":"Sally","age":45,"children":[{"name":"Kim","age":3},{"name":"Lee","age":1}]}}';
+
+// convertir cadena JSON a objeto JavaScript
+const jsObject = JSON.parse(jsonData);
+
+// actualizar objeto JavaScript
+jsObject.parent.age = 35;
+
+// convertir objeto JavaScript a cadena JSON
+const jsObjectToJson = JSON.stringify(jsObject);
+
+// muestra la cadena JSON
+console.log(jsObjectToJson);
